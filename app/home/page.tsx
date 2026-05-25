d67652b53aaff854cc60c96fb8152056210a5245
Moderator11 <p7925221@gmail.com>
2026-05-26 00:56:17 +0900

    chore(home, landing): home, landing 라우트 스왑

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function Landing() {
+export default function Home() {
   return (
     <div className="flex flex-1 items-center justify-center w-full p-5">
-      <h1 className="text-text-head-02 text-gray-800">시작 화면입니다.</h1>
+      <h1 className="text-text-head-02 text-gray-800">홈페이지 입니다.</h1>
     </div>
   );
 }
