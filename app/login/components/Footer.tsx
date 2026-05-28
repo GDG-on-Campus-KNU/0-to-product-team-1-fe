5be97beb69250fef8af1fec2087b213a0f1dde5d
Moderator11 <p7925221@gmail.com>
2026-05-28 16:01:32 +0900

    refactor: 로그인 페이지 컴포넌트 분리

---
+++ app/login/components/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <div className="flex flex-col gap-4 items-center justify-center pt-20 text-body-03">
+      <div className="w-1 h-14 bg-gradient-to-b from-stone-400 to-transparent"></div>
+      <span className="text-body-03 text-stone-400">MINDFUL MOMENT</span>
+    </div>
+  );
+}
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 import LoginForm from "./components/LoginForm";
 
 export default function Login() {
@@ -14,19 +16,7 @@ export default function Login() {
           className="object-cover -z-10"
         />
 
-        <div className="flex flex-col items-center justify-center pt-16 pb-8">
-          <div className="flex items-center justify-center w-32 h-32 rounded-full bg-green-500 border shadow-xs">
-            <Image
-              width={43}
-              height={42}
-              src="/hollow_leaves_icon.svg"
-              alt="hl_icon"
-            />
-          </div>
-
-          <h1 className="text-head-01">Step Back</h1>
-          <p className="text-body-01">정서적 쉼터로의 한 걸음</p>
-        </div>
+        <Header />
 
         <div className="flex flex-col flex-1 p-8 gap-8">
           <LoginForm />
@@ -36,12 +26,9 @@ export default function Login() {
               <span className="underline">회원가입 하기</span>
             </a>
           </div>
-
-          <div className="flex flex-col gap-4 items-center justify-center pt-20 text-body-03">
-            <div className="w-1 h-14 bg-gradient-to-b from-stone-400 to-transparent"></div>
-            <span className="text-body-03 text-stone-400">MINDFUL MOMENT</span>
-          </div>
         </div>
+
+        <Footer />
       </div>
     </div>
   );
