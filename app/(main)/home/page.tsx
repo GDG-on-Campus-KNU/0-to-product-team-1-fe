1d744f5127ce4eb384c7db2f727cff114c035085
nyeong <hannayoung1210@gmail.com>
2026-05-29 06:44:26 +0900

    feat: HomePage에 mockResult 적용

--- app/(main)/home/page.tsx
+++ app/(main)/home/page.tsx
@@ -1,13 +1,13 @@
-/*"use client";
+"use client";
 
-import React, { useState } from "react"; // 💡 useEffect 제거 완료
+import React, { useState } from "react";
 
-import { CrisisResultView } from "./components/CrisisResultView";
-import { DrillInputView } from "./components/DrillInputView";
-import { DrillLoadingView } from "./components/DrillLoadingView";
-import { DrillResultView } from "./components/DrillResultView";
+import { CrisisResultView } from "./components/view/CrisisResultView";
+import { DrillInputView } from "./components/view/DrillInputView";
+import { DrillLoadingView } from "./components/view/DrillLoadingView";
+import { DrillResultView } from "./components/view/DrillResultView";
 import type { CleanResult } from "./hooks/useGetDrill";
-import { MOCK_CRISIS } from "./mock/mockResult";
+import { MOCK_CRISIS, MOCK_DRILL } from "./mock/mockResult";
 
 type Step = "INPUT" | "LOADING" | "RESULT";
 
@@ -15,11 +15,9 @@ export default function Home() {
   const [currentStep, setCurrentStep] = useState<Step>("INPUT");
   const [resultData, setResultData] = useState<CleanResult | null>(null);
 
-  const TEST_IS_DRILL = false;
+  const TEST_IS_DRILL = true;
 
-  const handleInputSubmit = (text: string) => {
-    console.log("입력된 텍스트:", text); // 💡 text 미사용 에러 해결 완료
-
+  const handleInputSubmit = () => {
     setCurrentStep("LOADING");
 
     setTimeout(() => {
@@ -43,21 +41,6 @@ export default function Home() {
         ) : (
           <CrisisResultView data={resultData} />
         ))}
-    </div>
-  );
-}*/
-"use client";
-
-import React from "react";
-
-import { DrillResultView } from "./components/view/DrillResultView";
-import { MOCK_DRILL } from "./mock/mockResult"; // 💡 목데이터 경로만 맞춰주세요
-
-export default function Home() {
-  return (
-    <div className="flex flex-1 flex-col items-center w-full p-5">
-      {/* 🚨 에러 나는 컴포넌트 싹 다 치우고 위기 카드만 바로 렌더링! */}
-      <DrillResultView data={MOCK_DRILL} />
     </div>
   );
 }
