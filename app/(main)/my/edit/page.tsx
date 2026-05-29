cf4f7a7ce1779fe5debf707ca43d62fe9270c2ed
Moderator11 <p7925221@gmail.com>
2026-05-29 15:43:41 +0900

    feat: 프로필 대용 아이콘 추가

--- app/(main)/my/edit/page.tsx
+++ app/(main)/my/edit/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Image from "next/image";
 
 import ModifyForm from "../components/ModifyForm";
 import UserDeleteButton from "../components/UserDeleteButton";
@@ -6,7 +7,15 @@ import UserDeleteButton from "../components/UserDeleteButton";
 export default function Edit() {
   return (
     <div className="flex flex-col flex-1 bg-background p-8 gap-4">
-      <div className="flex flex-col items-center justify-center pt-16 pb-8">
+      <div className="flex flex-col gap-4 items-center justify-center pb-8">
+        <div className="flex items-center justify-center w-32 h-32 rounded-full bg-green-500 border shadow-xs">
+          <Image
+            width={43}
+            height={42}
+            src="/hollow_leaves_icon.svg"
+            alt="hl_icon"
+          />
+        </div>
         <h1 className="text-head-01">계정 정보 수정</h1>
         <h2 className="text-head-04">당신만의 온전한 공간을 가꾸어보세요.</h2>
       </div>
