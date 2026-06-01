3b96372cc6b84d05e17b15e3a23ea0f6d57edf79
Moderator11 <p7925221@gmail.com>
2026-06-01 16:30:25 +0900

    feat: DrillResultView에 entryData text 추가

--- app/(main)/home/components/view/DrillResultView.tsx
+++ app/(main)/home/components/view/DrillResultView.tsx
@@ -29,7 +29,7 @@ export function DrillResultView({ data, entryData }: DrillResultViewProps) {
         <h1 className="text-body-01 text-gray-700">
           Q. 오늘 가장 신경쓰였던 일이 무엇이었나요?
         </h1>
-        <TextCard text="오늘은 진행 중인 프로젝트의 디자인 시스템을 수정하는 과정에서 컴포넌트 구조를 어떻게 잡아야 할지 고민하느라 시간이 많이 쓰였습니다. 그래도 하나씩 정리되어 가는 느낌이라 뿌듯하네요!" />
+        <TextCard text={entryData.text} />
       </div>
       <div className="flex w-full flex-col gap-6">
         <h1 className="text-body-01 text-gray-700">
